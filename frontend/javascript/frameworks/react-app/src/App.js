a0e9c3f09cd3d2c0a3c8da85cb16173998ecac4d
Carlos VP <ca.varaperez@gmail.com>
2020-10-17 08:27:56 -0500

    Adding dynamic components

--- frontend/javascript/frameworks/react-app/src/App.js
+++ frontend/javascript/frameworks/react-app/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
