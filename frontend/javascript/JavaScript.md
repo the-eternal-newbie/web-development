5afdd8c4e5a03035dd6b542426cd108ec09a336d
Carlos VP <ca.varaperez@gmail.com>
2020-10-16 23:07:06 -0500

    Adding first custom component

---
+++ frontend/javascript/JavaScript.md
@@ -0,0 +1,5 @@
+# JavaScript
+
+It's a lightweight, cross-platform, object-oriented computer programming language; is one of the three core technologies of web development. It can be used in different places:
+- Client-side: Used in the browser
+- Server-side: Used on the server with node.js
--- frontend/javascript/frameworks/react-app/src/App.js
+++ frontend/javascript/frameworks/react-app/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person />
       </div>
     );
   }
